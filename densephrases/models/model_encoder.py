f6ec090fd19698ba483a1480fad810c01b60a091
Atem Aguer <atemjohn@stanford.edu>
2021-05-26 23:20:42 -0700

    model encoder created

---
+++ densephrases/models/model_encoder.py
@@ -0,0 +1,46 @@
+import json
+import argparse
+import torch
+import os
+import random
+import numpy as np
+import requests
+import logging
+import math
+import copy
+import wandb
+import string
+
+from time import time
+from tqdm import tqdm
+
+from densephrases.models import DensePhrases
+
+class DPHEncoder(DensePhrases):
+    def __init__(self, *args, **kwargs):
+        super(DPHEncoder, self).__init__(*args, **kwargs)
+        self.query_encoder = copy.deepcopy(self.phrase_encoder)
+
+    def embed_phrase(self, input_ids, attention_mask, token_type_ids):
+        """ Get phrase embeddings (token-wise) """
+        outputs_s = self.phrase_encoder(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+        )
+
+        sequence_output_s = outputs_s[0]
+        sequence_output_e = outputs_s[0]
+        start = sequence_output_s[:,:,:]
+        end = sequence_output_e[:,:,:]
+
+        return start, end
+
+    def embed_query(self, input_ids_, attention_mask_, token_type_ids_):
+        """ Get query start/end embeddings """
+
+        return self.query_encoder(
+            input_ids_,
+            attention_mask=attention_mask_,
+            token_type_ids=token_type_ids_,
+            )[0][:,:,:]
